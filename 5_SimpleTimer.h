de52bb2acd3369f836b09dd1b9e3f3a0c19d2d59
Vectorflex <chedser@gmail.com>
2023-10-08 11:27:56 +0300

    Create 5_SimpleTimer.h

---
+++ 5_SimpleTimer.h
@@ -0,0 +1,11 @@
+#include <chrono>
+#include <iostream>
+
+class SimpleTimer {
+public:
+	SimpleTimer();
+	~SimpleTimer();
+private:
+	std::chrono::time_point<std::chrono::steady_clock> start, end;
+	std::chrono::duration<float> duration;
+};
